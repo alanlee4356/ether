146ad2dc5d9d5be1d12ba597a3b453cd8a923e0a
alanlee4356 <alanlee4356@gmail.com>
2021-06-13 01:52:09 +0900

    solidity tutorial 41:55

--- student_test.sol
+++ student_test.sol
@@ -1,5 +1,28 @@
-pragma solidity 0.4.19;
+pragma solidity 0.5.1;
 
-contract sibal{
-    sdlkfjasdflksadjflkajds;
+contract MyContract {
+    uint256 public peopleCount = 0;
+    mapping(uint => Person) public people;
+    
+    address owner;
+    
+    modifier onlyOwner(){
+        require(msg.sender == owner); //함수호출자의정보가 owner와 일치하면 실행
+        _;
+    }
+    
+    struct Person {
+        uint _id;
+        string _firstName;
+        string _lastName;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner{
+        peopleCount +=1;
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
 }
