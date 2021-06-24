bf299db77afdcb2daf2592a73af4297e1a61ff49
이성현 <alanlee4356@gmail.com>
2021-06-24 10:57:10 +0900

    dasp top10 complete

---
+++ msgsender.sol
@@ -0,0 +1,31 @@
+pragma solidity >0.4.21 < 0.7.0;
+
+contract WithdrawalContract {
+    address public richest;
+    uint public mostSent;
+    
+    mapping (address=>uint) pendingWithdrawals;
+    
+    constructor() public payable{
+        richest = msg.sender;
+        mostSent = msg.value;
+    }
+    
+    function becomeRichest() public payable returns (bool){
+        if (msg.value>mostSent){
+            pendingWithdrawals[richest] += msg.value;
+            richest = msg.sender;
+            mostSent = msg.value;
+            
+            return true;
+            
+        } else{
+            return false;
+        }
+    }
+    function withdraw() public {
+        uint amount = pendingWithdrawals[msg.sender];
+        pendingWithdrawals[msg.sender] = 0;
+        msg.sender.transfer(amount);
+    }
+}
